87562dbcf1fc3985bfaf95d50de50afb290a0ba7
Hisyam <hisyamskurniawan18@gmail.com>
2023-01-22 01:31:37 +0700

    feat: add Products scenes component

--- src/scenes/products/index.jsx
+++ src/scenes/products/index.jsx
@@ -15,6 +15,70 @@ import {
 import Header from 'components/Header';
 import { useGetProductsQuery } from 'features/apiSlice';
 
+const Product = (data) => {
+  const theme = useTheme();
+  const { _id, name, description, price, rating, category, supply, stat } =
+    data.product;
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <Card
+      sx={{
+        backgroundImage: 'none',
+        backgroundColor: theme.palette.background.alt,
+        borderRadius: '0.55rem',
+      }}
+    >
+      <CardContent>
+        <Typography
+          sx={{ fontSize: '14px' }}
+          color={theme.palette.secondary[700]}
+          gutterBottom
+        >
+          {category}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography
+          sx={{ marginBottom: '1.5rem' }}
+          color={theme.palette.secondary[400]}
+        >
+          ${Number(price).toFixed(2)}
+        </Typography>
+        <Rating value={rating} readOnly />
+        <Typography variant="body2">{description}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant="primary"
+          size="small"
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          SEE MORE
+        </Button>
+      </CardActions>
+      <Collapse
+        in={isExpanded}
+        timeout="auto"
+        unmountOnExit
+        sx={{ color: theme.palette.neutral[300] }}
+      >
+        <CardContent>
+          <Typography>id: {_id}</Typography>
+          <Typography>Supply Left: {supply}</Typography>
+          <Typography>
+            Yearly Sales This Year: {stat.yearlySalesTotal}
+          </Typography>
+          <Typography>
+            Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
+
 const Products = () => {
   const { data, isLoading, isSuccess } = useGetProductsQuery();
   const isNonMobile = useMediaQuery('(min-width: 1000px)');
@@ -23,7 +87,21 @@ const Products = () => {
     <Box margin="1.5rem 2.5rem">
       <Header title="Products" subTitle="See your list of products" />
       {isSuccess && data && !isLoading ? (
-        <Box>Hello</Box>
+        <Box
+          marginTop="20px"
+          display="grid"
+          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          justifyContent="space-between"
+          rowGap="20px"
+          columnGap="1.33%"
+          sx={{
+            '& > div': { gridColumn: isNonMobile ? undefined : 'span 4' },
+          }}
+        >
+          {data.map((product) => (
+            <Product product={product} key={product._id} />
+          ))}
+        </Box>
       ) : (
         <Box>Loading...</Box>
       )}

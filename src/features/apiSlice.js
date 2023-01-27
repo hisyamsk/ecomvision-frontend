a1e9cbf0e3393fd1315aaab39cf84c61a624f0df
Hisyam <hisyamskurniawan18@gmail.com>
2023-01-27 23:11:32 +0700

    feat: admin scene

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -3,7 +3,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: 'api',
-  tagTypes: ['User', 'Products', 'Customers', 'Transactions', 'Geography', 'Sales'],
+  tagTypes: [
+    'User',
+    'Products',
+    'Customers',
+    'Transactions',
+    'Geography',
+    'Sales',
+    'Admins',
+  ],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -33,6 +41,10 @@ export const api = createApi({
       query: () => 'sales/sales',
       providesTags: ['Sales'],
     }),
+    getAdmins: build.query({
+      query: () => 'management/admins',
+      providesTags: ['Admins'],
+    }),
   }),
 });
 
@@ -43,4 +55,5 @@ export const {
   useGetTransactionsQuery,
   useGetGeographyQuery,
   useGetSalesQuery,
+  useGetAdminsQuery,
 } = api;
---
+++ src/scenes/admin/index.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Box, useTheme } from '@mui/material';
+import { useGetAdminsQuery } from 'features/apiSlice';
+import { DataGrid } from '@mui/x-data-grid';
+
+import Header from 'components/Header';
+import DataGridCustomCulumnMenu from 'components/DataGridCustomColumnMenu';
+
+const Admin = () => {
+  const { data, isLoading } = useGetAdminsQuery();
+  const theme = useTheme();
+
+  const columns = [
+    {
+      field: '_id',
+      headerName: 'ID',
+      flex: 1,
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 0.5,
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 1,
+    },
+    {
+      field: 'phoneNumber',
+      headerName: 'Phone Number',
+      flex: 0.5,
+      renderCell: (params) => {
+        params.value.replace(/^(\d{3})(\d{3})(\d{4})/, '($1)$2-$3');
+      },
+    },
+    {
+      field: 'country',
+      headerName: 'Country',
+      flex: 0.4,
+    },
+    {
+      field: 'occupation',
+      headerName: 'Occupation',
+      flex: 1,
+    },
+    {
+      field: 'role',
+      headerName: 'Role',
+      flex: 0.5,
+    },
+  ];
+
+  return (
+    <Box margin="1.5rem 2.5rem">
+      <Header title="Admins" subTitle="Managing admins and list of admins" />
+      <Box
+        marginTop="40px"
+        height="75vh"
+        sx={{
+          '& .MuiDataGrid-root': {
+            border: 'none',
+          },
+          '& .MuiDataGrid-cell': {
+            borderBottom: 'none',
+          },
+          '& .MuiDataGrid-columnHeaders': {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: 'none',
+          },
+          '& .MuiDataGrid-virtualScroller': {
+            backgroundColor: theme.palette.primary.light,
+          },
+          '& .MuiDataGrid-footerContainer': {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: 'none',
+          },
+          '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          columns={columns}
+          rows={data || []}
+          getRowId={(row) => row._id}
+          components={{
+            ColumnMenu: DataGridCustomCulumnMenu,
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Admin;
